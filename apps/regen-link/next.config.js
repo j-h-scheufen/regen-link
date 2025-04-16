db3d2e6e8cb80214ed6dd618717a062427335874
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2025-04-16 16:10:24 -0400

    chore: finished UI setup and changed landing page
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- apps/regen-link/next.config.js
+++ apps/regen-link/next.config.js
@@ -4,14 +4,37 @@ const nextConfig = {
   poweredByHeader: false, // Remove X-Powered-By header for security
   images: {
     formats: ['image/avif', 'image/webp'], // Enable modern image formats
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   webpack(config) {
-    // SVGR configuration
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
+    // Handle SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
--- apps/regen-link/tailwind.config.js
+++ apps/regen-link/tailwind.config.js
@@ -1,16 +1,98 @@
 // tailwind.config.js
 const { heroui } = require('@heroui/react');
+const { withTV } = require('tailwind-variants/transformer');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withTV({
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // make sure it's pointing to the ROOT node_module as HeroUI is being hoisted
     '../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '480px',
+      ...defaultTheme.screens, // 'sm': '640px', 'md': '768px', 'lg': '1024px', 'xl': '1280px', '2xl': '1536px'
+    },
+    extend: {
+      fontFamily: {
+        // You can add custom fonts here if needed
+      },
+    },
   },
   darkMode: 'class',
-  plugins: [heroui()],
-};
+  plugins: [
+    heroui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: '#79D91F',
+              50: '#4A9107',
+              100: '#4F960D',
+              200: '#59A116',
+              300: '#66AE23',
+              400: '#71B92E',
+              500: '#7DC639',
+              600: '#8FD84B',
+              700: '#A3E664',
+              800: '#B7F180',
+              900: '#D3FAAE',
+              foreground: '#000000',
+            },
+            secondary: {
+              DEFAULT: '#aa825c',
+              50: '#15100b',
+              100: '#2a2117',
+              200: '#55412e',
+              300: '#947251',
+              400: '#aa825c',
+              500: '#bf9368',
+              600: '#d8ac81',
+              700: '#e1bf9d',
+              800: '#ead1b9',
+              900: '#f2e3d5',
+              foreground: '#000000',
+            },
+          },
+        },
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: '#7FD92A',
+              50: '#1F3A06',
+              100: '#2D520A',
+              200: '#3E6F11',
+              300: '#538F1B',
+              400: '#69AE29',
+              500: '#7DC639',
+              600: '#8FD84B',
+              700: '#A3E664',
+              800: '#B7F180',
+              900: '#D3FAAE',
+              foreground: '#000000',
+            },
+            secondary: {
+              DEFAULT: '#D4A373',
+              50: '#15100b',
+              100: '#2a2117',
+              200: '#55412e',
+              300: '#947251',
+              400: '#aa825c',
+              500: '#bf9368',
+              600: '#d8ac81',
+              700: '#e1bf9d',
+              800: '#ead1b9',
+              900: '#f2e3d5',
+              foreground: '#000000',
+            },
+          },
+        },
+      },
+    }),
+    require('@tailwindcss/typography'),
+  ],
+});
